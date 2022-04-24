28be4102ccc4750791adc52376c325c361481add
EXPITC <indratc18@gmail.com>
2022-04-25 02:31:56 +0700

    add config to graphQL
    make single return graphQL api with message:'Hello GraphQL!'

--- BookServer/server.js
+++ BookServer/server.js
@@ -1,4 +1,28 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+
 const app = express();
 
-app.listen(5000.,()=> console.log('http:localhost:5000'))
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'HelloGraphQL',
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello GraphQL!',
+      },
+    }),
+  }),
+});
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  }),
+);
+
+const port = 5000;
+app.listen(port, () => console.log('http:localhost:' + port));
